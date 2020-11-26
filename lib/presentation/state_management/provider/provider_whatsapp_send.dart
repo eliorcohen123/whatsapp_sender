f40d039b436024e23015eb573bc4931bed5d06e4
EliorCohen <elior123456123123@gmail.com>
2020-11-26 16:11:29 +0200

    update

--- lib/presentation/state_management/provider/provider_whatsapp_send.dart
+++ lib/presentation/state_management/provider/provider_whatsapp_send.dart
@@ -1,4 +1,3 @@
-import 'package:clipboard/clipboard.dart';
 import 'package:contacts_service/contacts_service.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
